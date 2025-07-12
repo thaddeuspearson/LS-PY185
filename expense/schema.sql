c13e57cbdadf9808659b34fcab972a4f18ba40b7
Thaddeus <thaddeuspearson@gmail.com>
2025-07-12 18:44:34 +0200

    + add expense functionality to expense

--- expense/schema.sql
+++ expense/schema.sql
@@ -4,3 +4,8 @@ CREATE TABLE expenses (
     memo text NOT NULL,
     created_on date NOT NULL
 );
+
+INSERT INTO expenses (amount, memo, created_on)
+       VALUES (14.56, 'Pencils', NOW()),
+              (3.29, 'Coffee', NOW()),
+              (49.99, 'Text Editor', NOW());
