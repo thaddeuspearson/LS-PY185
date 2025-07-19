826eeadcdcc00542943f1338b99ec89a4c5e7aad
Thaddeus <thaddeuspearson@gmail.com>
2025-07-20 00:08:05 +0200

    + type annotations to SessionPersistence methods

--- todos_app_optimized/todos/session_persistence.py
+++ todos_app_optimized/todos/session_persistence.py
@@ -9,11 +9,11 @@ class SessionPersistence:
         if 'lists' not in self.session:
             self.session['lists'] = []
 
-    def all_lists(self):
-        """Gets all lists iin the ciurrent session"""
+    def all_lists(self) -> list:
+        """Gets all lists in the current session"""
         return self.session['lists']
 
-    def create_list(self, title):
+    def create_list(self, title: str) -> None:
         """Creates a new todo list"""
         self.all_lists().append({
             "id": str(uuid4()),
@@ -22,14 +22,14 @@ class SessionPersistence:
         })
         self.session.modified = True
 
-    def update_list(self, list_id, title):
+    def update_list(self, list_id: int, title: str) -> None:
         """Updates the given todo list"""
         lst = self.find_list(list_id)
         if lst:
             lst["title"] = title
         self.session.modified = True
 
-    def delete_list(self, list_id):
+    def delete_list(self, list_id: int) -> None:
         """Deletes the given todo list"""
         self.session["lists"] = [
             lst for lst in self.session["lists"]
@@ -44,7 +44,7 @@ class SessionPersistence:
             None
         )
 
-    def create_todo(self, todo_list, todo_title):
+    def create_todo(self, todo_list: list, todo_title: str) -> None:
         """Creates a todo in the given todo_list"""
         todo_list["todos"].append({
             "id": str(uuid4()),
@@ -53,7 +53,7 @@ class SessionPersistence:
         })
         self.session.modified = True
 
-    def delete_todo(self, todo_list, todo_id):
+    def delete_todo(self, todo_list: list, todo_id: int) -> None:
         """Deletes the todo with the given todo_id from the given todo_list"""
         todo_list["todos"] = [
             todo for todo in todo_list["todos"]
@@ -61,12 +61,12 @@ class SessionPersistence:
         ]
         self.session.modified = True
 
-    def update_todo_status(self, todo, is_completed):
+    def update_todo_status(self, todo: dict, is_completed: bool) -> None:
         """Sets the given todo's completed status to True"""
         todo["completed"] = is_completed
         self.session.modified = True
 
-    def mark_all_todos_completed(self, todo_list):
+    def mark_all_todos_completed(self, todo_list: list) -> None:
         """Sets all todos completed status to True"""
         for todo in todo_list["todos"]:
             todo["completed"] = True
