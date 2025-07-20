e021fbce810f70109359960b7bdbc202658e9445
Thaddeus <thaddeuspearson@gmail.com>
2025-07-21 00:19:50 +0200

    + logging and display functionality to DatabasePersistence module

--- todos_app_optimized/todos/database_persistence.py
+++ todos_app_optimized/todos/database_persistence.py
@@ -1,10 +1,17 @@
 """Custom DatabasePersistence class for the Todo App"""
 import sys
 from contextlib import contextmanager
+from textwrap import dedent
 from typing import Generator
-from psycopg2 import connect, OperationalError
+import logging
+from psycopg2 import connect, DatabaseError, InterfaceError, OperationalError
 from psycopg2.extras import DictCursor
 from psycopg2.extensions import connection as PGConnection
+
+
+LOG_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
+logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+logger = logging.getLogger(__name__)
 
 
 class DatabasePersistence:
@@ -31,9 +38,38 @@ class DatabasePersistence:
             if connection:
                 connection.close()
 
+    @contextmanager
+    def _database_cursor(self):
+        """
+        Creates a cursor context manager for the given database
+        """
+        try:
+            with self._database_connect() as connection:
+                with connection.cursor(cursor_factory=DictCursor) as cursor:
+                    yield cursor
+        except InterfaceError as e:
+            print(e)
+            sys.exit(1)
+
     def all_lists(self) -> list:
-        """Gets all lists iin the current session"""
-        pass
+        """Gets all lists in the current session"""
+        query = dedent("""
+            SELECT * FROM lists
+        """)
+        logger.info("Executing query: %s", query)
+        try:
+            with self._database_cursor() as cursor:
+                cursor.execute(query)
+                results = cursor.fetchall()
+        except DatabaseError as e:
+            print(e)
+
+        lists = [dict(row) for row in results]
+
+        for lst in lists:
+            lst.setdefault('todos', [])
+
+        return lists
 
     def create_list(self, title: str) -> None:
         """Creates a new todo list"""
@@ -49,7 +85,21 @@ class DatabasePersistence:
 
     def find_list(self, todo_list_id: str) -> dict | None:
         """finds and returns the list associated with the given id or None"""
-        pass
+        query = dedent("""
+            SELECT * FROM lists WHERE id = %s
+        """)
+        logger.info(
+            "Executing query: %s with todo_list_id: %s", query, todo_list_id
+        )
+        try:
+            with self._database_cursor() as cursor:
+                cursor.execute(query, (todo_list_id,))
+                lst = dict(cursor.fetchone())
+        except DatabaseError as e:
+            print(e)
+
+        lst.setdefault('todos', [])
+        return lst
 
     def create_todo(self, todo_list: dict, todo_title: str) -> None:
         """Creates a todo in the given todo_list"""
