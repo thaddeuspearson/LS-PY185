018bd98984bcaf21da20372522793459229e47e0
Thaddeus <thaddeuspearson@gmail.com>
2025-07-21 19:17:17 +0200

    pep8 fixes, general code clean-up

--- todos_app_optimized/todos/database_persistence.py
+++ todos_app_optimized/todos/database_persistence.py
@@ -15,58 +15,58 @@ logger = logging.getLogger(__name__)
 
 
 class DatabasePersistence:
-    """Handles database persistence for todo app"""
+    """Handles database persistence for todo app."""
     DBNAME = "todos"
 
     def __init__(self) -> None:
-        """Initiates the DatabasePersistence class"""
+        """Initiates the DatabasePersistence class."""
         self.dbname = DatabasePersistence.DBNAME
 
     @contextmanager
     def _database_connect(self) -> Generator[PGConnection, None, None]:
-        """Obtains a connection to the indicated PostgreSQL database"""
+        """Obtains a connection to the indicated PostgreSQL database."""
         connection = None
         try:
-            connection = connection = connect(dbname=self.dbname)
+            connection = connect(dbname=self.dbname)
             connection.autocommit = True
             with connection:
                 yield connection
         except OperationalError:
-            print(f"Unable to get a connection to: {self.dbname}. Exiting.")
+            logger.exception(
+                f"Unable to get a connection to: {self.dbname}. Exiting."
+            )
             sys.exit(1)
         finally:
             if connection:
                 connection.close()
 
     @contextmanager
-    def _database_cursor(self):
-        """Creates a cursor context manager for the given database"""
+    def _database_cursor(self) -> Generator[DictCursor, None, None]:
+        """Creates a cursor context manager for the given database."""
         try:
             with self._database_connect() as connection:
                 with connection.cursor(cursor_factory=DictCursor) as cursor:
                     yield cursor
         except InterfaceError as e:
-            print(e)
+            logger.exception(e)
             sys.exit(1)
 
-    def all_lists(self) -> list:
-        """Gets all lists in the current session"""
+    def all_lists(self) -> list[dict]:
+        """Gets all lists in the current session."""
 
         query = dedent("""
             SELECT * FROM lists
         """)
 
-        logger.info(
-            "Executing query: %s",
-            query
-        )
+        logger.info("Executing query: %s", query)
 
         try:
             with self._database_cursor() as cursor:
                 cursor.execute(query)
                 results = cursor.fetchall()
         except DatabaseError as e:
-            print(e)
+            logger.exception(e)
+            return []
 
         lists = [dict(row) for row in results]
 
@@ -77,106 +77,99 @@ class DatabasePersistence:
         return lists
 
     def create_list(self, title: str) -> None:
-        """Creates a new todo list"""
+        """Creates a new todo list."""
 
         query = dedent("""
             INSERT INTO lists (title) VALUES (%s)
         """)
 
-        logger.info(
-            "Executing query: %s with title: %s",
-            query, title
-        )
+        logger.info("Executing query: %s with title: %s", query, title)
 
         try:
             with self._database_cursor() as cursor:
                 cursor.execute(query, (title,))
         except DatabaseError as e:
-            print(e)
+            logger.exception(e)
 
     def update_list(self, list_id: int, new_title: str) -> None:
-        """Updates the given todo list"""
+        """Updates the given todo list."""
 
         query = dedent("""
             UPDATE lists SET title = %s WHERE id = %s
         """)
 
-        logger.info(
-            "Executing query: %s with new title: %s and id: %s",
-            query, new_title, list_id
-        )
+        logger.info("Executing query: %s with new title: %s and id: %s",
+                    query, new_title, list_id)
 
         try:
             with self._database_cursor() as cursor:
                 cursor.execute(query, (new_title, list_id))
         except DatabaseError as e:
-            print(e)
+            logger.exception(e)
 
     def delete_list(self, list_id: int) -> None:
-        """Deletes the given todo list"""
+        """Deletes the given todo list."""
 
         query = dedent("""
             DELETE from lists WHERE id = %s
         """)
 
-        logger.info(
-            "Executing query: %s with id: %s",
-            query, list_id
-        )
+        logger.info("Executing query: %s with id: %s", query, list_id)
 
         try:
             with self._database_cursor() as cursor:
                 cursor.execute(query, (list_id,))
         except DatabaseError as e:
-            print(e)
+            logger.exception(e)
 
-    def _find_todos_for_list(self, todo_list_id: int) -> list:
-        """Finds all todos associated with the given todo_list_id"""
+    def _find_todos_for_list(self, todo_list_id: int) -> list[dict]:
+        """Finds all todos associated with the given todo_list_id."""
 
         query = dedent("""
             SELECT * FROM todos WHERE list_id = %s
         """)
 
-        logger.info(
-            "Executing query: %s with list_id %s",
-            query, todo_list_id
-        )
+        logger.info("Executing query: %s with list_id %s", query, todo_list_id)
 
         try:
             with self._database_cursor() as cursor:
                 cursor.execute(query, (todo_list_id,))
                 todos = cursor.fetchall()
         except DatabaseError as e:
-            print(e)
+            logger.exception(e)
+            return []
 
         todos = [dict(todo) for todo in todos]
         return todos
 
     def find_list(self, todo_list_id: str) -> dict | None:
-        """finds and returns the list associated with the given id or None"""
+        """Finds and returns the list associated with the given id or None."""
 
         query = dedent("""
             SELECT * FROM lists WHERE id = %s
         """)
 
-        logger.info(
-            "Executing query: %s with todo_list_id: %s",
-            query, todo_list_id
-        )
+        logger.info("Executing query: %s with todo_list_id: %s",
+                    query, todo_list_id)
 
         try:
             with self._database_cursor() as cursor:
                 cursor.execute(query, (todo_list_id,))
-                lst = dict(cursor.fetchone())
+                result = cursor.fetchone()
+                if result is None:
+                    return None
+                else:
+                    lst = dict(result)
         except DatabaseError as e:
-            print(e)
+            logger.exception(e)
+            return None
 
         todos = self._find_todos_for_list(todo_list_id)
         lst.setdefault('todos', todos)
         return lst
 
     def create_todo(self, todo_title: str, todo_list_id: int) -> None:
-        """Creates a todo in the given todo_list"""
+        """Creates a todo in the given todo_list."""
 
         query = dedent("""
             INSERT INTO todos (title, list_id) VALUES (%s, %s)
@@ -184,23 +177,23 @@ class DatabasePersistence:
 
         logger.info(
             "Executing query: %s with todo_title: %s and todo_list_id: %s",
-            query, todo_title, todo_list_id,
+            query, todo_title, todo_list_id
         )
 
         try:
             with self._database_cursor() as cursor:
                 cursor.execute(query, (todo_title, todo_list_id))
         except DatabaseError as e:
-            print(e)
+            logger.exception(e)
 
     def delete_todo(self, todo_list: list, todo_id: int) -> None:
-        """Deletes the todo with the given todo_id from the given todo_list"""
+        """Deletes the todo with the given todo_id from the given todo_list."""
         pass
 
     def update_todo_status(self, todo: dict, is_completed: bool) -> None:
-        """Sets the given todo's completed status to True"""
+        """Sets the given todo's completed status to True."""
         pass
 
     def mark_all_todos_completed(self, todo_list: dict) -> None:
-        """Sets all todos completed status to True"""
+        """Sets all todos completed status to True."""
         pass
